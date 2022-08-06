ba0964f2dbb17b2578c4f974f1daac73f988b61f
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-08-06 10:35:39 -0300

    feat: add initial structure router, schema validation and handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
+import router from "./routers/router.js";
+import handleErrorsMiddleware from "./middlewares/handleErrorMiddleware.js";
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(router);
+app.use(handleErrorsMiddleware);
 
 export default app;
